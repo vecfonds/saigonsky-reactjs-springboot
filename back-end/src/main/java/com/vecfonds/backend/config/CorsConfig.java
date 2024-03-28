3f84147a51d2655f2cc901a3e737ab6b1315bfce
Nguyễn Tiến Dũng <92855884+vecfonds@users.noreply.github.com>
2024-03-29 03:40:03 +0700

    Update CorsConfig.java

--- back-end/src/main/java/com/vecfonds/backend/config/CorsConfig.java
+++ back-end/src/main/java/com/vecfonds/backend/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://saigonsky.netlify.app/")
+                .allowedOrigins("https://saigonsky.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
